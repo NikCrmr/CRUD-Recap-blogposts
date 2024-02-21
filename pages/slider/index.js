2236ff0fa2731a1b144b5f190e3a316f9d49a93b
Niko Cremer <n.ko@posteo.de>
2024-02-21 22:00:45 +0100

    add EMoji and change it with the type range slider

--- pages/slider/index.js
+++ pages/slider/index.js
@@ -6,24 +6,45 @@ export default function Slider() {
   const handleRangeChange = (event) => {
     setRangeValue(event.target.value);
   };
+  function Emoji() {
+    const decNumber = rangeValue / 2.55;
+    console.log("decNumber", decNumber);
+    if (decNumber <= 41) {
+      return <StyledEmoji>😢</StyledEmoji>;
+    }
+    if (decNumber >= 42 && decNumber <= 45 && decNumber < 55) {
+      return <StyledEmoji>😇</StyledEmoji>;
+    }
+    if (decNumber > 55) {
+      return <StyledEmoji>🥳</StyledEmoji>;
+    }
+  }
+  console.log("rangeValue", rangeValue);
   return (
     <>
       <label>How do you feel about this website?</label>
       <input
         type="range"
         min={0}
-        max={100}
+        max={255}
         value={rangeValue}
         onChange={handleRangeChange}
       />
       <StyledColorRange rangeValue={rangeValue}></StyledColorRange>
+      <Emoji />
     </>
   );
 }
 
 const StyledColorRange = styled.div`
-  background-color: ${(props) =>
-    `rgb(${255 - props.rangeValue}, ${props.rangeValue}, 0)`};
+  background-color: ${
+    (props) => `hsl(${(props.rangeValue / 255) * 120}, 100%, 50%)`
+    //`rgb(${255 - props.rangeValue}, ${props.rangeValue}, 0)`
+  };
   height: 100px;
   width: 80vh;
+`;
+
+const StyledEmoji = styled.span`
+  font-size: 4rem;
 `;
